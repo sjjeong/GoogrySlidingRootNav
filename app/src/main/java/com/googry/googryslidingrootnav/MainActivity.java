cab99bd2ae35b2b4e76f1b6ba978f95df4946399
Googry <sjjeong1225@gmail.com>
2017-08-13 13:33:16 +0900

    Initial commit

---
+++ app/src/main/java/com/googry/googryslidingrootnav/MainActivity.java
@@ -0,0 +1,27 @@
+package com.googry.googryslidingrootnav;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        new SlidingRootNavBuilder(this)
+                .withMenuLayout(R.layout.sliding_root_nav)
+                .withToolbarMenuToggle(toolbar)
+                .inject();
+    }
+}
